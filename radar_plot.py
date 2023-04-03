f46e54524cb3173643a88bfe7ea0e1edfd27516d
Ibrahim <ibrahim@ibrahim.net>
2023-04-03 12:13:44 +0200

    Added cartopy

--- radar_plot.py
+++ radar_plot.py
@@ -1,5 +1,14 @@
-#File to plot radar image with matplotlib and cartopy
+"""
+File to plot radar image with matplotlib and cartopy
+"""
 
 import numpy as np
 import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
 
+#let's create some data and play with it
+rand_data=np.random.uniform(low=0.5, high=10, size=(100,100))
+
+print(rand_data)
+plt.plot(rand_data)
+plt.show()
